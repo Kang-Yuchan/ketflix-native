20be8d69044738ce0b8a991bc2f331cf49fbfe96
Yuchan <mikuplayer123@gmail.com>
2020-06-30 14:35:55 +0900

    cache images

--- App.tsx
+++ App.tsx
@@ -1,29 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { AppLoading } from 'expo';
+import { Text, Image } from 'react-native';
+import { Asset } from 'expo-asset';
+
+const cacheImages = (images: Array<string>) =>
+	images.map((image: string) => {
+		if (typeof image === 'string') {
+			return Image.prefetch(image);
+		} else {
+			return Asset.fromModule(image).downloadAsync();
+		}
+	});
 
 export default function App() {
-	const [ count, setCount ] = React.useState(0);
-	const onPressCount = React.useCallback(
-		() => {
-			setCount(count + 1);
-		},
-		[ count ]
-	);
-	return (
-		<View style={styles.container}>
-			<Text>{count}</Text>
-			<Button onPress={onPressCount} title="count" color="#841584" accessibilityLabel="COUNT BUTTON" />
-			<StatusBar style="auto" />
-		</View>
+	const [ isReady, setIsReady ] = React.useState<boolean>(false);
+	const loadAssets = async () => {
+		const images = cacheImages([
+			'https://images.unsplash.com/photo-1593359393721-8c301de4bf7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+			require('./assets/splash.png')
+		]);
+		console.log(images);
+	};
+
+	const onFinish = () => setIsReady(true);
+
+	const onError = () => console.error;
+
+	return isReady ? (
+		<Text>App is Ready</Text>
+	) : (
+		<AppLoading startAsync={loadAssets} onFinish={onFinish} onError={onError} />
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
-});
