dd8247cdefd9c2d4ef9b08f4fb90b2cac2afd79a
Yuchan <mikuplayer123@gmail.com>
2020-07-01 22:21:03 +0900

    bullet proof

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -7,11 +7,37 @@ type HomeProps = {
 	navigation: NavigationStackProp<{}>;
 };
 
+type MoviesState = {
+	nowPlaying: Array<{}>;
+	nowPlayingError: string | null;
+	popular: Array<{}>;
+	popularError: string | null;
+	upComing: Array<{}>;
+	upComingError: string | null;
+};
+
 const Movies: React.FC<HomeProps> = ({ navigation }: HomeProps) => {
+	const [ movies, setMovies ] = React.useState<MoviesState>({
+		nowPlaying: [],
+		nowPlayingError: null,
+		popular: [],
+		popularError: null,
+		upComing: [],
+		upComingError: null
+	});
+
 	const getApiData = async () => {
 		const [ nowPlaying, nowPlayingError ] = await moviesAPI.nowPlaying();
 		const [ popular, popularError ] = await moviesAPI.popular();
 		const [ upComing, upComingError ] = await moviesAPI.upComing();
+		setMovies({
+			nowPlaying,
+			nowPlayingError,
+			popular,
+			popularError,
+			upComing,
+			upComingError
+		});
 	};
 
 	React.useEffect(() => {
@@ -20,7 +46,7 @@ const Movies: React.FC<HomeProps> = ({ navigation }: HomeProps) => {
 
 	return (
 		<View style={{ flex: 1, backgroundColor: 'black' }}>
-			<Text>Movies</Text>
+			<Text style={{ color: "white" }}>{movies.nowPlaying?.length}</Text>
 		</View>
 	);
 };
